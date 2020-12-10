262106e69d2204373850a5564e6819c3d1f0a14a
Ajay Paliwal <ajaypaliwalcto@outlook.com>
2020-12-10 13:00:42 +0530

    Upload Design
    Chirag

--- src/app/currentinventory/component/upload(1)/upload.component.ts
+++ src/app/currentinventory/component/upload(1)/upload.component.ts
@@ -1,4 +1,9 @@
-import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter, Input } from '@angular/core';
+import { selectSelectedTenantId, selectSelectedTenant } from '../../../store/selectors/tenant.selectors';
+import { EventService } from '../../../dynamic-events/service/event.service';
+import { finalize } from 'rxjs/operators';
+import { AuthService } from '../../../core/auth.service';
+import { NgxSpinnerService } from 'ngx-spinner';
 
 @Component({
   selector: 'app-upload',
@@ -9,12 +14,43 @@ export class UploadComponent implements OnInit {
 
 
   @Output() hideClose = new EventEmitter();
+  @Input() EventList: any;
+  public uploadActivity: any;
+  public selectedTenantId: number;
+  public busy: boolean;
 
-  constructor() { }
+  constructor(private authService: AuthService, private spinner: NgxSpinnerService, private eventService: EventService) { }
 
   ngOnInit(): void {
+    debugger;
+    this.selectedTenantId = parseInt(localStorage.getItem('TenantId'));
+    this.GetEvents();
   }
   Close() {
     this.hideClose.emit(false);
   }
+  GetEvents() {
+    debugger;
+    this.eventService.GetEvents(this.selectedTenantId, this.authService.accessToken)
+      .pipe(finalize(() => {
+        this.busy = false;
+        this.spinner.hide();
+      })).subscribe(result => {
+        if (result.entity != null) {
+          debugger;
+          this.EventList = result.entity;
+
+          // this.EventList.forEach(element => {
+          //   let obj = {}
+          //   obj = JSON.parse(element.circumstanceJsonString);
+
+          //   this.checklist.forEach(element12 => {
+          //     element12.isSelected = obj[element12.columnName];
+          //   });
+
+          // });
+        }
+      })
+  }
+
 }
